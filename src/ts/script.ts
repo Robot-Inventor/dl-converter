4fca7300065131c2b41682ccb57bb49002f5c004
Koki Tanaka <mail@gojiteji.com>
2024-03-27 13:58:13 +0900

    素数表現の機能追加 (#4)
    * add prime number conversion
    
    * update file size
    
    * fixed darkmode css
    
    * rm .DS_Store

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,10 +1,13 @@
 import "@material/web/textfield/outlined-text-field.js";
 import "@material/web/button/outlined-button.js";
 import "@material/web/button/filled-button.js";
+import "@material/web/switch/switch.js";
 import type { MdOutlinedTextField } from "@material/web/textfield/outlined-text-field.js";
 import type { MdOutlinedButton } from "@material/web/button/outlined-button.js";
 import type { MdFilledButton } from "@material/web/button/filled-button.js";
-import table from "../json/table.json";
+import type { MdSwitch } from "@material/web/switch/switch.js";
+import slangTable from "../json/table.json";
+import primeTable from "../json/prime-table.json";
 
 const userInput = document.querySelector<MdOutlinedTextField>("#user-input");
 if (!userInput) {
@@ -26,17 +29,48 @@ if (!resultTextBox) {
     throw new Error("Result text box not found");
 }
 
-const encode = (input: string): string => {
-    // キーを長い順にソート
-    // 長いものから置換することで、長いもの（例：催眠音声）が誤って短いもの（例：催眠）として置換されることを防ぐ
-    const keys = Object.keys(table).sort((a, b) => b.length - a.length) as Array<keyof typeof table>;
+const primeSwitch = document.querySelector<MdSwitch>("#prime-switch");
+if (!primeSwitch) {
+    throw new Error("Prime switch not found");
+}
 
-    for (const key of keys) {
-        const value = table[key];
-        if (typeof value === "string") {
-            input = input.replaceAll(key, value);
-        } else {
-            input = input.replaceAll(key, value[0]);
+const encode = (input: string): string => {
+    if (primeSwitch.selected) {
+        const keys = Object.keys(primeTable).sort((a, b) => b.length - a.length) as Array<keyof typeof primeTable>;
+        let output = 1;
+        for (const key of keys) {
+            const value = primeTable[key];
+            if (typeof value === "number") {
+                if (input.includes(key)) {
+                    output *= value;
+                    input = input.replaceAll(key, value.toString());
+                }
+            } else {
+                value.forEach((v) => {
+                    if (input.includes(key)) {
+                        output *= v;
+                        input = input.replaceAll(key, v.toString());
+                    }
+                }
+                );
+            }
+        }
+        input = output.toString();
+
+
+    } else {
+
+        // キーを長い順にソート
+        // 長いものから置換することで、長いもの（例：催眠音声）が誤って短いもの（例：催眠）として置換されることを防ぐ
+        const keys = Object.keys(slangTable).sort((a, b) => b.length - a.length) as Array<keyof typeof slangTable>;
+
+        for (const key of keys) {
+            const value = slangTable[key];
+            if (typeof value === "string") {
+                input = input.replaceAll(key, value);
+            } else {
+                input = input.replaceAll(key, value[0]);
+            }
         }
     }
 
@@ -44,21 +78,48 @@ const encode = (input: string): string => {
 };
 
 const decode = (input: string): string => {
-    // キーを長い順にソート
-    // 長いものから置換することで、長いもの（例：催眠音声）が誤って短いもの（例：催眠）として置換されることを防ぐ
-    const keys = Object.keys(table).sort((a, b) => b.length - a.length) as Array<keyof typeof table>;
+    if (primeSwitch.selected) {
+        const inputNumber = parseInt(input, 10);
+        if (isNaN(inputNumber) || inputNumber < 1) {
+            return "自然数を入力してください";
+        }
+        let decodedText = "";
+        const keys = Object.keys(primeTable).sort((a, b) => b.length - a.length) as Array<keyof typeof primeTable>;
+        for (const key of keys) {
+            const value = primeTable[key];
+            if (typeof value === "number") {
+                if (inputNumber % value === 0) {
+                    decodedText += key;
+                }
+            } else {
+                value.forEach((v) => {
+                    if (inputNumber % v === 0) {
+                        decodedText += key;
+                    }
+                });
+            }
+        }
+        if (decodedText === "") {
+            decodedText = "健全なテキストです";
+        }
+        input = decodedText;
 
-    for (const key of keys) {
-        const value = table[key];
-        if (typeof value === "string") {
-            input = input.replaceAll(value, key);
-        } else {
-            value.forEach((v) => {
-                input = input.replaceAll(v, key);
-            });
+    } else {
+        // キーを長い順にソート
+        // 長いものから置換することで、長いもの（例：催眠音声）が誤って短いもの（例：催眠）として置換されることを防ぐ
+        const keys = Object.keys(slangTable).sort((a, b) => b.length - a.length) as Array<keyof typeof slangTable>;
+
+        for (const key of keys) {
+            const value = slangTable[key];
+            if (typeof value === "string") {
+                input = input.replaceAll(value, key);
+            } else {
+                value.forEach((v) => {
+                    input = input.replaceAll(v, key);
+                });
+            }
         }
     }
-
     return input;
 };
 
@@ -69,3 +130,4 @@ encodeButton.addEventListener("click", () => {
 decodeButton.addEventListener("click", () => {
     resultTextBox.value = decode(userInput.value);
 });
+
